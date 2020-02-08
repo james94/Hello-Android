bcbd6f03d9bd2ae13dd27086d52c97bad01b8296
james94 <11840449+james94@users.noreply.github.com>
2020-02-08 12:05:12 -0800

    Implemented Java Code for Android App Functionality
    First creates layout for app user interface inside
    onCreate(...) {...} method. Implemented showTag()
    method that creates a toast message letting the user
    know what is happening on the screen. Implemented
    makeItRain() method for when a user clicks on
    makeItRain button, increment money by 1000, set
    money text color to yellow if money counter is >=
    10000, display a positive message on saving money
    if mouney counter >= 50000.

--- app/src/main/java/com/guzmanx/makeitrain/MainActivity.java
+++ app/src/main/java/com/guzmanx/makeitrain/MainActivity.java
@@ -2,13 +2,69 @@ package com.guzmanx.makeitrain;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
+//    private Button showMoney;
+//    private Button showTag;
+
+
+    private TextView moneyText;
+    private TextView savingText;
+    private int moneyCounter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        moneyText = findViewById(R.id.money_text);
+        savingText = findViewById(R.id.saving_text);
+//        showMoney = findViewById(R.id.button_make_rain);
+//        showTag = findViewById(R.id.button_show_tag);
+
+//        showMoney.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Log.d("MYTAG", "onClick: Show Money");
+//            }
+//        });
+    }
+
+    public void showTag(View view) {
+        // Create toast message, small text that pop up on screen to let user know what happening
+        // pass the context, the text, the duration for showing the message on the screen
+        Toast.makeText(getApplicationContext(), R.string.app_name, Toast.LENGTH_LONG)
+                .show();
+
+        // Debugging
+        //Log.d("MYTAG", "onClick: Show Money");
+    }
+
+    public void makeItRain(View v) {
+        // Convert android number to currency specific to android's region in the world
+        NumberFormat numberFormat = NumberFormat.getCurrencyInstance(); // US ex: $1,000.00
+
+        // Give a visual cue to user they are saving a lot of money
+        // checks if moneyCounter gets big 10,000, then change text color
+        moneyCounter += 1000;
+        if(moneyCounter >= 10000) {
+            moneyText.setTextColor(Color.YELLOW);
+        }
+        // check if moneyCounter gets big, msg "Woah, you're getting richer"
+        if(moneyCounter >= 50000) {
+            savingText.setText("Woah, keeping saving!");
+        }
+        moneyText.setText(String.valueOf(numberFormat.format(moneyCounter)));
+
+        // Once you know things are working, get rid of log debugging
+        // Log.d("MIR", "makeItRain: Tapped " + moneyCounter);
     }
 }
